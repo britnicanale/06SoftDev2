326ab8be33a807db8418a22dbf8272e8e2558eda
Britni <bcanale@stuy.edu>
2019-01-30 20:50:30 -0500

    FINISHED WORK YAY

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,18 +1,39 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 
-var b = document.getElementByTagName("button");
+var b = document.getElementById("clear");
 b.addEventListener('click',
   function(e){
-    //ctx.clearRect();
+    ctx.clearRect(0,0,600,600);
   console.log(e)
   }
 );
 
+var shape = "dot";
+
+
 c.addEventListener('click',
   function(e){
-    ctx.fillRect(e.clientX, e.clientY, 50,50);
-    console.log(e.clientX)
+    if(shape == "dot"){
+      ctx.beginPath();
+      ctx.ellipse(e.clientX, e.clientY, 30,30, 0, 0, 2*Math.PI, true);
+      ctx.fill();
+    }else{
+      ctx.fillRect(e.clientX, e.clientY, 10,10);
+    }
     console.log(e)
+  }
+);
+
+var s = document.getElementById("shape");
+s.addEventListener('click',
+  function(e){
+    if(shape == "box"){
+      shape = "dot";
+      s.innerHTML = "Rectangle"
+    }else{
+      shape = "box";
+      s.innerHTML = "Dot"
+    }
   }
 );
