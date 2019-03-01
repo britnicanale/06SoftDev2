b31f77c0ee2733a7b6fd105e3cc4549389620235
Britni <bcanale@stuy.edu>
2019-03-01 00:39:46 -0500

    YAYYYY

--- 06_mongo/mongo.py
+++ 06_mongo/mongo.py
@@ -34,30 +34,34 @@ def zipbadgrade(zip, grade):
 def distanceFrom(lat, long, dist):
     food = collection.find()
     for place in food:
-        if distance(place["address"]["coord"][0], place["address"]["coord"][1], lat, long) < dist:
-            print(place)
+        #print(place["address"]["coord"])
+        if place["address"]["coord"] != []:
+            distanceB = distance(place["address"]["coord"][1], place["address"]["coord"][0], lat, long)
+            if distanceB < dist:
+                print(place["name"])
+                print(distanceB)
 
 
 #Haservine formula from https://www.movable-type.co.uk/scripts/latlong.html
 def distance(latOne, longOne, latTwo, longTwo):
     a = math.sin(math.radians(latTwo - latOne)/2)**2 + math.cos(latOne) * math.cos(latTwo)* math.sin(math.radians(longTwo - longOne)/2)**2
-    print("A")
-    print(a)
+    #print("A")
+    #print(a)
     if( a < 0):
         c = 2 * math.atan(math.sqrt(1-a))
     elif (a > 1):
         c = 2 * math.atan(math.sqrt(a))
     else:
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    print("C")
-    print(c)
+    #print("C")
+    #print(c)
     radius = 3958.761
     distance = radius * c
-    print("D")
-    print(distance)
+    #print("D")
+    #print(distance)
     return distance
 
 
 distance(0, 0, 25, 25)
-distanceFrom(40.7178, 74.0139, 0.5)
+distanceFrom(40.7178, -74.0139, 0.5)
 #zipcode("10282")
