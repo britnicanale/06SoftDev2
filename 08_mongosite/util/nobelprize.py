b6d53a93ccc6aced657969cb168d4e30e2fc2b9c
Britni <bcanale@stuy.edu>
2019-03-08 12:11:54 -0500

    uh oh

--- 08_mongosite/util/nobelprize.py
+++ 08_mongosite/util/nobelprize.py
@@ -1,17 +1,14 @@
-# Team huMONGOus : Britni Canale & Jabir Chowdhury
+# Team Lo-Chew : Britni Canale & Ahnaf Hasan & Alex Lin
 # Softdev pd6
-# K#07: Import/Export Bank
-# 2019-03-04
+# K#8: Ay Mon, Go Git It From Yer Flask
+# 2019-03-07
 
 '''
 dataset name: Nobel Prizes
-
 description: Contains a record of all nobel prizes given
 in the years 1901-2018 in the categories: physics, chemistry, medicine,
 peace, economics, and literature.
-
 Link to dataset: http://api.nobelprize.org/v1/prize.json
-
 Import mechanism:
    1) saved the contents of the dataset in nobelprize.json
    2) created a function called insertData which opens nobelprize.json
@@ -25,17 +22,16 @@ Import mechanism:
 from pymongo import MongoClient
 import json
 
-SERVER_ADDR='206.189.75.99' # change to IP you want to start with
+SERVER_ADDR='142.93.126.17' # change to IP you want to start with
 client = MongoClient(SERVER_ADDR, 27017)
 db = client.huMONGOus
 collection = db.nobelprize
 
 def changeIP(ip):
     SERVER_ADDR = ip
-    print(ip)
 
-def getIP():
-    return SERVER_ADDR
+def isEmpty():
+    return huMONGOus.nobelprize.find() == []
 
 def insertData():
     '''
@@ -93,7 +89,6 @@ def find_topic(topic):
     return ret
 
 # insertData() #already called, no need to call again
-
 
 # print("###########################")
 # print("testing find_year()")
