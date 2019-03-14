7875a9fca95f3775c364dc389f1e530ca9a6890a
Britni <bcanale@stuy.edu>
2019-03-13 22:33:52 -0400

    YAY WORKS

--- 10_svg/svg.js
+++ 10_svg/svg.js
@@ -6,6 +6,8 @@
 var pic = document.getElementById("vimage");  //Getting SVG
 
 
+//==========================EVENT LISTENERS ===========================
+
 //Clear Button!
 var b = document.getElementById("clear");
 b.addEventListener('click',
@@ -16,19 +18,22 @@ b.addEventListener('click',
   }
 );
 
-var circles = document.getElementsByTagName("circle");
-for(i = 0; i < circles.length; i++){
-  circles[i].addEventListener('click',
+var addCirc = function(c){
+  c.addEventListener('click',
     function(e){
-      if (circles[i].getAttribute("fill") == "blue"){
-        circles[i].setAttribute("fill", "green");
+      console.log("Circle")
+      e.preventDefault()
+      if (c.getAttribute("fill") == "blue"){
+        console.log("circle is alrady blue")
+        toGreen(c);
       }else{
-        circles[i].setAttribute("x", Math.random() * 500);
-        circles[i].setAttribute("x", Math.random() * 500);
+        toRandom(c);
       }
-    }
+    } 
   );
 }
+
+
 
 //Adds dot with line connecting if svg is clicked
 pic.addEventListener('click', function(e){
@@ -36,21 +41,38 @@ pic.addEventListener('click', function(e){
       //Offset gives difference of pixels between event point and reference point, which is upper left corner of the element in question, the canvas
       var x = e.offsetX;
       var y = e.offsetY;
-      dot(x,y);
+      dot(x,y, e);
       console.log(e);
 })
 
-//=================== DRAW & CLEAR FUNCTIONS =====================
-var dot = function(x, y){
+//========================== DRAW & CLEAR FUNCTIONS ==========================
+var dot = function(x, y, e){
   var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  addCirc(c)
+  if(e.target['nodeName']=='circle'){
+    return;
+  }
+  c.setAttribute("r", 10);
   c.setAttribute("cx", x);
   c.setAttribute("cy", y);
-  c.setAttribute("r", 10);
-  c.setAttribute("fill", "blue");
   c.setAttribute("stroke", "black");
+  c.setAttribute("fill", "blue");
   pic.appendChild(c);
 }
 
+
+var toGreen = function(c){
+  console.log("in green")
+  c.setAttribute("fill", "green");
+}
+
+var toRandom = function(c){
+  let newX = 10 + Math.random() * 480;
+  let newY = 10 + Math.random() * 480;
+  c.setAttribute("cx", newX);
+  c.setAttribute("cy", newY);
+  c.setAttribute("fill", "blue");
+}
 
 var clear = function(){
   var c = document.createElementNS("http://www.w3.org/2000/svg", "rect");
