f720652d24bbd4d3b559d2ec233c9735d42c9bcc
Britni <bcanale@stuy.edu>
2019-03-18 22:34:57 -0400

    Making progress...

--- 11_svg/svg.js
+++ 11_svg/svg.js
@@ -8,38 +8,46 @@ var mov = document.getElementById("move");
 var xVel = 1;
 var yVel = 1;
 var requestID = 0;
+var dots = pic.childNodes
 
 //==========================EVENT LISTENERS ===========================
 
 //Clear Button!
 var b = document.getElementById("clear");
-b.addEventListener('click',
-  function(e){
-    e.preventDefault();//in place to avoid default functionfrom event if there is a default
-    clear()
-    console.log(e);
-  }
-);
+b.addEventListener('click', function(e){
+  e.preventDefault();
+  clear();
+
+});
 
 mov.addEventListener('click',
   function(e){
     e.preventDefault();
     if(pic.hasChildNodes()){
-      dots = pic.childNodes;
-      console.log(dots)
-      for( i = 0; i < dots.length; i++){
+      //dots = pic.childNodes;
+      console.log("circles: " + dots)
+      move()
+      /*for( i = 0; i < dots.length; i++){
         let cx = parseInt(dots[i].getAttribute('cx'));
         let cy = parseInt(dots[i].getAttribute('cy'));
         let color = dots[i].getAttribute('fill')
-        console.log(cx)
-        console.log(color)
-        move(cx, cy, color)
-      }
+        console.log("cx: " +cx)
+        console.log("color: " +color)
+        move()
+      }*/
     }
     console.log(e);
   }
 
 )
+
+var stop = document.getElementById("stop");
+stop.addEventListener('click', function(e){
+    e.preventDefault();
+    window.cancelAnimationFrame(requestID);
+  }
+);
+
 
 var addCirc = function(c){
   c.addEventListener('click',
@@ -81,29 +89,53 @@ var dot = function(x, y, color){
   c.setAttribute("stroke", "black");
   c.setAttribute("fill", color);
   pic.appendChild(c);
-  console.log(c)
+  console.log("circle being added" + c)
+  return c;
 }
 
-var move = function(x, y, color){
-  console.log(x)
-  clear();
+var move = function(){
+  dots = pic.childNodes;
+  console.log(dots)
   window.cancelAnimationFrame(requestID);
-  console.log(requestID);
-  if ((x >= pic.width - 10 || x <= pic.width + 10) && !requestID == 0){ //checks for bounce
+  console.log("request ID: " + requestID);
+  for(i = 0; i < dots.length; i++){
+    console.log(dots[0])
+    console.log("moving!")
+    let x = parseInt(dots[0].getAttribute('cx'))
+    let y = parseInt(dots[0].getAttribute('cy'))
+    let color = dots[0].getAttribute("fill")
+    if ((x >= 490 || x <= 10) && !requestID == 0){ //checks for bounce
+      xVel = xVel * -1; //reverses velocity
+      console.log("bounce")
+    }
+    if ((y >= 490 || y <= 10) && !requestID == 0){ //checks for bounce
+      yVel = yVel * -1; //reverses velocity
+      console.log("bounce")
+    }
+    console.log("x: " +x)
+    console.log("y: " +y)
+    console.log("color: " +color)
+    x = x + xVel; //shows motion
+    y = y + yVel; //shows motion
+    clearOne(dots[0])
+    dot(x, y, color)
+  }/*
+  if ((x >= pic.width - 10 || x <= 10) && !requestID == 0){ //checks for bounce
     xVel = xVel * -1; //reverses velocity
     console.log("bounce")
   }
-  if ((y == pic.height - 10 || y <= pic.width + 10) && !requestID == 0){ //checks for bounce
+  if ((y >= pic.height - 10 || y <= 10) && !requestID == 0){ //checks for bounce
     yVel = yVel * -1; //reverses velocity
     console.log("bounce")
   }
   x = x + xVel; //shows motion
   y = y + yVel; //shows motion
-  console.log(x)
-  console.log(y)
-  console.log(color)
-  dot(x, y, color)
-  requestID = window.requestAnimationFrame(move(x, y, color));
+
+  console.log("x: " +x)
+  console.log("y: " +y)
+  console.log("color: " +color)*/
+  //dot(x, y, color)
+  requestID = window.requestAnimationFrame(move);
 }
 
 var toGreen = function(c){
@@ -123,5 +155,11 @@ var clear = function(){
   window.cancelAnimationFrame(requestID);
   while(pic.hasChildNodes()){
     pic.removeChild(pic.childNodes[pic.childNodes.length-1])
+  }
+}
+
+var clearOne = function(c){
+  if(pic.hasChildNodes()){
+    pic.removeChild(c)
   }
 }
